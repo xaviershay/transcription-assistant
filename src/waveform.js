5c99a58dee051d080c3a8f0b073611c9dde69d7f
Xavier Shay <contact@xaviershay.com>
2026-07-18 13:04:52 +1000

    refactor: move Timeline plugin ownership out of waveform.js

--- src/waveform.js
+++ src/waveform.js
@@ -1,13 +1,8 @@
 import WaveSurfer from 'wavesurfer.js'
 import RegionsPlugin from 'wavesurfer.js/plugins/regions'
-import TimelinePlugin from 'wavesurfer.js/plugins/timeline'
 
 export function createWaveSurfer(container) {
   const regions = RegionsPlugin.create()
-  const timeline = TimelinePlugin.create({
-    height: 20,
-    style: { color: '#e6e6e6', fontSize: '10px' },
-  })
 
   const wavesurfer = WaveSurfer.create({
     container,
@@ -17,7 +12,7 @@ export function createWaveSurfer(container) {
     height: 120,
     minPxPerSec: 50,
     sampleRate: 44100,
-    plugins: [regions, timeline],
+    plugins: [regions],
   })
 
   return { wavesurfer, regions }
