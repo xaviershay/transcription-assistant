f19fcd398ba2954af01cf74d535f4603e8d99c15
Xavier Shay <contact@xaviershay.com>
2026-08-02 14:52:18 +1000

    Add spectrogram view synced to the main waveform

--- src/waveform.js
+++ src/waveform.js
@@ -1,8 +1,19 @@
 import WaveSurfer from 'wavesurfer.js'
 import RegionsPlugin from 'wavesurfer.js/plugins/regions'
+import SpectrogramPlugin from 'wavesurfer.js/plugins/spectrogram'
 
-export function createWaveSurfer(container) {
+export function createWaveSurfer(container, spectrogramContainer) {
   const regions = RegionsPlugin.create()
+  const spectrogram = SpectrogramPlugin.create({
+    container: spectrogramContainer,
+    height: 200,
+    labels: true,
+    scale: 'logarithmic',
+    frequencyMin: 27.5,
+    frequencyMax: 4186,
+    colorMap: 'roseus',
+    useWebWorker: true,
+  })
 
   const wavesurfer = WaveSurfer.create({
     container,
@@ -12,8 +23,8 @@ export function createWaveSurfer(container) {
     height: 120,
     minPxPerSec: 50,
     sampleRate: 44100,
-    plugins: [regions],
+    plugins: [regions, spectrogram],
   })
 
-  return { wavesurfer, regions }
+  return { wavesurfer, regions, spectrogram }
 }
